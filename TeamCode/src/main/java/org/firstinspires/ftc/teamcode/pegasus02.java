ba90f3a26f47f9e1a04a8ef3242e920cca97c36b
ZjpffrotaZ <frotatv@gmail.com>
2022-01-26 15:46:03 -0300

    thanks for acess my project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pegasus02.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pegasus02.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -14,17 +15,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 public class pegasus02 extends LinearOpMode {
 
     // Declaração dos componentes físicos
+
+    DcMotor arm;
     DcMotor take;
     DcMotor fl;
     DcMotor fr;
+
+    ServoImplEx deposit;
+    ServoImplEx duck;
+
     BNO055IMU imu;
 
     /* Declaração das variaveis que vão receber os valores do joystick do gamepad, X e Y,
        configurando os parâmetros do imu .
        Estamos usando o IMU para termos ideia de como vamos usar os seus valores para
        realizar o algoritimo PID, que será usado no período autônomo.*/
+
     double setmode = 1;
-    double x, y, orientation, right, left, speed;
+    double setmode2 = 1;
+    double x, y, orientation, right, left, speed,up, down;
     final BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 
     @Override
@@ -32,11 +41,14 @@ public class pegasus02 extends LinearOpMode {
 
         //Comandos para o robô reconhecer o hardware.
 
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu  = hardwareMap.get(BNO055IMU.class, "imu");
 
+        duck = hardwareMap.get(ServoImplEx.class, "duck");
+
+        arm  = hardwareMap.get(DcMotor.class, "arm");
         take = hardwareMap.get(DcMotor.class, "take");
-        fl = hardwareMap.get(DcMotor.class, "fl");
-        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl   = hardwareMap.get(DcMotor.class, "fl");
+        fr   = hardwareMap.get(DcMotor.class, "fr");
 
         //Aqui estamos definindo os parâmetros que nos interessa.
 
@@ -78,10 +90,34 @@ public class pegasus02 extends LinearOpMode {
                 if (setmode % 2 == 0){          //denovo o sistema é desligado.
                     while (speed <= 1.0) {
                         sleep(30);        //Estamos usando esse "while" e essa seuquência de
-                        speed = speed + 0.01;         //cáuculos para reduzir o torque do motor quando ele
+                        speed += 0.01;               //cáuculos para reduzir o torque do motor quando ele
                         take.setPower(speed);}         //acionado e assim reduzindo o desgaste.
-                }else{
-                    speed=0;}
+                }else{ speed=0;}
+            }
+
+            // ACIONA SERVO DO PATO
+            if(gamepad1.y){                 //Quando precionado o sistema liga e se manteém,
+                setmode2++;                  // quando precionado novamente desliga.
+                if (setmode2 % 2 == 0){
+                    while (true) {
+                        duck.setPosition(1.0);}
+                }else{ duck.setPosition(0);}
+            }
+
+            // ACIONA O MOTOR DO BRAÇO
+            if(gamepad1.dpad_up){          //Se precionarmos o dpad para cima o braço levanta,
+                arm.setPower(0.7);          // e se precionarmos para baixo, ele abaixa.
+            }
+            if(gamepad1.dpad_down){
+                arm.setPower(-0.7);
+            }
+
+            //ACIONA O DEPOSITO
+            if (gamepad1.dpad_left){
+                deposit.setPosition(-0.6);
+            }
+            if (gamepad1.dpad_right){
+                deposit.setPosition(0.6);
             }
 
             Orientation orientation = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
